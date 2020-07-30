e7cc0fe8f396615bf4d1d53ebe25dd04d0dfd117
Bontu380 <onyldrm@hotmail.com>
2020-07-30 20:15:57 +0300

    Update PlayerRotations.cs

--- Assets/PlayerRotations.cs
+++ Assets/PlayerRotations.cs
@@ -7,7 +7,7 @@ public class PlayerRotations : MonoBehaviour
     private Rigidbody2D playerRb;
     private SpringJoint2D joint;
     private float angle;
-    private float smoothFactor = 3f;
+    private float smoothFactor = 1f;
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
@@ -16,17 +16,18 @@ public class PlayerRotations : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
+      /*
         Vector2 velocity = playerRb.velocity;
         angle = Mathf.Atan2(velocity.y,velocity.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         */
 
+        
         Vector2 velocity = playerRb.velocity;
         angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
         
         Quaternion rotated = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.rotation = Quaternion.Lerp(transform.rotation, rotated,smoothFactor );
-
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotated,smoothFactor * Time.deltaTime );
+        
     }
 }
