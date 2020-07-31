7dc084600b663fe29432939e17340c9961446c89
Bontu380 <onyldrm@hotmail.com>
2020-07-31 12:40:55 +0300

    Update CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,43 +5,65 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Camera cam;
-    float originalSize;
-    float zoomOutSize;
-    float time;
-    /*void Start()
-    {
-        time = 3.5f;
-        originalSize = cam.orthographicSize;
-        zoomOutSize = originalSize + 6f;
-  
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        zoomOut();
-    }
-
-    public void zoomOut()
-    {
-        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize,zoomOutSize, time * Time.deltaTime);
-    }*/
+    private float originalSize;
+    private float zoomOutSize;
+    private float zoomSmoothTime = 3.5f;
+    public float countDownTime = 3f;
+    
 
     void Start()
     {
-        time = 3.5f;
         originalSize = cam.orthographicSize;
         zoomOutSize = originalSize + 6f;
-        StartCoroutine(zoomOut());
+
+        Coroutine waitForZoomOut = StartCoroutine(zoomOut());
+        Coroutine waitForZoomIn = StartCoroutine(zoomIn(waitForZoomOut));
+        //StartCoroutine(countdownToStart());
+        
     }
+
+/*
+    private void Update()
+    {
+        
+    }
+
+*/
 
     public IEnumerator zoomOut()
     {
-        while(cam.orthographicSize < zoomOutSize)
+        while(cam.orthographicSize <= zoomOutSize - 0.001f)
         {
-            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, zoomOutSize, time * Time.deltaTime);
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, zoomOutSize, zoomSmoothTime * Time.deltaTime);
             yield return null;
         }
         yield return new WaitForSeconds(1.5f);
     }
+
+    public IEnumerator zoomIn(Coroutine waitForZoomOut)
+    {
+
+        yield return waitForZoomOut;
+
+        while (cam.orthographicSize >= originalSize - 0.001f)
+        {
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, originalSize, zoomSmoothTime * Time.deltaTime);
+            yield return null;
+        }
+        yield return new WaitForSeconds(1.5f);
+    }
+
+    /*public IEnumerator countdownToStart()
+    {
+        float localTime = 3f;
+
+        while ()
+        {
+            yield return null;
+        }
+
+        GameController.instance.start = true;
+        yield return null;
+    }*/
+
 }
