eb330cb1b736649caa991bd3a3db0784feae5829
Bontu380 <onyldrm@hotmail.com>
2020-08-04 11:45:33 +0300

    Moving object

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class MovingObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    public float moveSpeed = 0.8f;
+
+    public bool movingToPos1 = true;
+
+    private Vector3 pos1;
+    private Vector3 pos2;
+
+    public float xOffset = 3.5f;
+    public float yOffset = 0f;
+
+    private void Start()
+    {
+        pos1 = transform.position + new Vector3(xOffset,yOffset,0f);
+        pos2 = transform.position + new Vector3(-xOffset, -yOffset, 0f);
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (movingToPos1)
+        {
+            transform.Translate(pos1 * Time.deltaTime * moveSpeed);
+
+            float distanceToPos = Vector3.Distance(pos1, transform.position);
+
+            if (distanceToPos <= 0.2f)
+            {
+                movingToPos1 = false;
+            }
+        }
+        else if(!movingToPos1) //O zaman pos2 ye gidiyordur.
+        {
+            transform.Translate(pos2 * Time.deltaTime * moveSpeed);
+
+            float distanceToPos = Vector3.Distance(transform.position,pos2);
+            if (distanceToPos <= 0.2f)
+            {
+                movingToPos1 = true;
+            }
+
+        }
+
     }
 }
